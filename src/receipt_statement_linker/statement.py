315b97ec0ca111ba71a4a12d9c1e355670b6ef27
Rehan <rehan@rehanrana.com>
2025-05-31 19:13:54 -0400

    Add statement.py
    Don't know how I missed this

---
+++ src/receipt_statement_linker/statement.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+from .classification import Classifications
+
+
+class Transaction(BaseModel):
+    name: str
+    datetime: datetime
+    withdrawl_amount: float | None
+    deposit_amount: float | None
+    classification: Classifications
+
+
+class TranscribedStatement(BaseModel):
+    opening_balance: float | None
+    transactions: list[Transaction]
+    closing_balance: float | None
+
+
+class TranscribedStatements(BaseModel):
+    transcribed_statements: list[TranscribedStatement]
